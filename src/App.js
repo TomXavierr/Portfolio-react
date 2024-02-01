2f98ac1752fd0f5871c39707d80b6d5768267cab
Tom Xavier <tomxavieronthego@gmail.com>
2024-02-01 22:58:14 +0530

    created navbar component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import CssBaseline from "@mui/material/CssBaseline";
 
 import Navbar from "./components/Navbar";
+import { Container } from "@mui/material";
 
 function App() {
     return (
         <div className="App">
             <CssBaseline>
-                <Navbar />
+                <Container maxWidth="lg">
+                    <Navbar />
+                </Container>
+                
             </CssBaseline>
-           
         </div>
     );
 }
